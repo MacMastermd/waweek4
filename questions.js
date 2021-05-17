dc2e4eba748b84d458247adc5a74b46ec822b9cb
stamoshair <macmastermd@gmail.com>
2021-05-17 01:14:05 -0600

    questions.js

---
+++ questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()s", "function myFunction() ", "function::+1", "function[]"],
+        answer: "function = myFunction()s"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+      title: "What is the purpose of console.log?",
+      choices: ["prompt a user", "to split wood", "alert a user", "print text to console"],
+      answer: "print text to console"
+   },
+   {
+      title: "Which of these is not used to loop?",
+      choices: ["for", "while", "foreach", "sequence"],
+      answer: "sequence"
+    },
+   {
+      title: "Which of these is not a way to save a variable",
+      choices: ["vet", "var", "let", "const"],
+      answer: "vet"
+   },
+   {
+      title: "How do you check if your code is working in the browser",
+      choices: ["console log", "asking the teacher", "dev tools", "guessing"],
+      answer: "console log"
+   }
+  ];
